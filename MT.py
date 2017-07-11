5865ec2a8f1d8a710e6c2bef9aa306f861fb8500
hyoungjoo <hyoungjoo@oddconcepts.kr>
2017-07-11 18:28:02 +0900

    구구단 Default Flag 추가

--- MT.py
+++ MT.py
@@ -1,15 +1,21 @@
-def Multi_Table(reverse_order_flag):
+def Multi_Table(reverse_order_flag=False):
     for i in range(1,10):
         for j in range(1,10):
-            if int(reverse_order_flag):
+            if reverse_order_flag:
                 print("%d X %d = %d" % ((10-i), (10-j), (10-i)*(10-j)))
             else:
                 print("%d X %d = %d" % (i, j, i*j))
-    print(reverse_order_flag)
+
 
 reverse_order_flag = input("거꾸로 출력 하시겠습니까?")
 
-try:
+if reverse_order_flag:
     Multi_Table(reverse_order_flag)
-except ValueError:
-    print("정수 값을 입력해주세요")
+else:
+    Multi_Table()
+
+#Multi_Table(reverse_order_flag)
+#try:
+#    Multi_Table(reverse_order_flag)
+#except ValueError:
+#    print("정수 값을 입력해주세요")
