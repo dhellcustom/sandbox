ff6df619297b0772587d58f2217a2ea5eb86afc3
hyoungjoo <hyoungjoo@oddconcepts.kr>
2017-07-11 16:39:37 +0900

    구구단 Flag Exception 추가

--- MT.py
+++ MT.py
@@ -1,11 +1,15 @@
 def Multi_Table(reverse_order_flag):
     for i in range(1,10):
         for j in range(1,10):
-            if reverse_order_flag:
+            if int(reverse_order_flag):
                 print("%d X %d = %d" % ((10-i), (10-j), (10-i)*(10-j)))
             else:
                 print("%d X %d = %d" % (i, j, i*j))
     print(reverse_order_flag)
 
 reverse_order_flag = input("거꾸로 출력 하시겠습니까?")
-Multi_Table(reverse_order_flag)
+
+try:
+    Multi_Table(reverse_order_flag)
+except ValueError:
+    print("정수 값을 입력해주세요")
